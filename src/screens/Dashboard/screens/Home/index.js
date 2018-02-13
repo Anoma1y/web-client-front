82355fdfb3f0d5d736eabccb0c2d1dd312f0e380
German <ueuknjg@gmail.com>
2018-02-13 15:13:37 +0300

    added grid

--- src/screens/Dashboard/screens/Home/index.js
+++ src/screens/Dashboard/screens/Home/index.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import { Grid, Container } from 'semantic-ui-react'
 
 const Home = () => (
-    <div>
-        DASHBOARD / HOME
-    </div>
+    <Container>
+        <Grid>
+            <Grid.Row centered>
+                <Grid.Column width={8}>
+                    <Grid.Row>
+
+                    </Grid.Row>
+                    <Grid.Row>
+
+                    </Grid.Row>
+                </Grid.Column>
+                <Grid.Column width={4}>
+                    <Grid.Row>
+
+                    </Grid.Row>
+                </Grid.Column>
+            </Grid.Row>
+        </Grid>
+    </Container>
 );
 
 export default Home;
