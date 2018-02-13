e366ba22afe24ec0a25d5410476a6b64851cc0cc
Alexander Svintsov <mrcarter@iMac-Alexander.local>
2018-02-13 20:33:49 +0300

    Add request card

--- src/screens/Dashboard/screens/Home/index.js
+++ src/screens/Dashboard/screens/Home/index.js
@@ -1,19 +1,21 @@
 import React from 'react'
 import { Grid, Container } from 'semantic-ui-react'
 
+import Request from 'components/Request'
+
 const Home = () => (
     <Container>
         <Grid>
             <Grid.Row centered>
-                <Grid.Column width={8}>
+                <Grid.Column width={10}>
                     <Grid.Row>
-
+                        <Request />
                     </Grid.Row>
                     <Grid.Row>
 
                     </Grid.Row>
                 </Grid.Column>
-                <Grid.Column width={4}>
+                <Grid.Column width={6}>
                     <Grid.Row>
 
                     </Grid.Row>
