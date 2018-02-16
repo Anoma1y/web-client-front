9e1075c3788d547fd60103d54abb579fa762174d
Vlad Kamyshanov <vladkamyshanov@gmail.com>
2018-02-16 16:50:47 +0300

    Pre merge fix

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,10 +4,12 @@ import { routerReducer } from 'react-router-redux'
 import RequestsReducer from './RequestsReducer'
 import CalculatorReducer from './CalculatorReducer'
 import RoadmapReducer from "./RoadmapReducer";
+import TimerReducer from './TimerReducer'
 
 export default combineReducers({
     routing: routerReducer,
     requests: RequestsReducer,
     calculator: CalculatorReducer,
-    roadmap: RoadmapReducer
+    roadmap: RoadmapReducer,
+    timer: TimerReducer
 })
