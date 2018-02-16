29dba6e49a10ed17cde6f11495152ad31a310fe7
German <ueuknjg@gmail.com>
2018-02-16 15:13:41 +0300

    initial commit

---
+++ src/reducers/RoadmapReducer.js
@@ -0,0 +1,11 @@
+const INITIAL_STATE = {
+    progressBar: 47.54
+
+};
+
+export default (state = INITIAL_STATE, action) => {
+    switch (action.type) {
+        default:
+            return state
+    }
+}
