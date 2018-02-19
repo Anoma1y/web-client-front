6514a57e19d7962026c6540d3f88fd184252674d
German <ueuknjg@gmail.com>
2018-02-19 17:06:41 +0300

    refactoring code

--- src/components/SocialNetwork/index.js
+++ src/components/SocialNetwork/index.js
@@ -2,8 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import {
     Grid,
-    Segment,
-    Icon
+    Segment
 } from 'semantic-ui-react'
 import SocialNetworkItem from './SocialNetworkItem'
 
