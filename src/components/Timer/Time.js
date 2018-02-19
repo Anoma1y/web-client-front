ba7f86c24b9a5a73b5130643f7d751c572ccd918
German <ueuknjg@gmail.com>
2018-02-19 17:47:27 +0300

    add mobile queries for timer

--- src/components/Timer/Time.js
+++ src/components/Timer/Time.js
@@ -6,7 +6,7 @@ import {
 
 export default ({timeName, value}) => {
     return (
-        <Grid.Column width={2}>
+        <Grid.Column widescreen={2} computer={2} tablet={4} mobile={4}>
             <Card.Meta as={"h2"}>{timeName}</Card.Meta>
             <Card.Description as={"h1"}>{value}</Card.Description>
         </Grid.Column>
