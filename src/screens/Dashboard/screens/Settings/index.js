3eb1c40f3bfc30ebf1682776d4795ba6de087a44
German <ueuknjg@gmail.com>
2018-02-19 16:36:16 +0300

    add vertically reversed for settings page

--- src/screens/Dashboard/screens/Settings/index.js
+++ src/screens/Dashboard/screens/Settings/index.js
@@ -9,31 +9,31 @@ const Settings = () => (
     <Container>
         <Grid>
             <Grid.Row centered>
+                <Grid reversed={"tablet vertically mobile vertically"} >
+                    <Grid.Column widescreen={5} computer={5} tablet={16} mobile={16} >
+                        <Grid>
 
-                <Grid.Column widescreen={5} computer={5} tablet={16} mobile={16} >
-                    <Grid>
+                            <Grid.Column widescreen={16} computer={16} tablet={8} mobile={16}>
+                                <Grid.Row>
+                                    <ChangePassword/>
+                                </Grid.Row>
+                            </Grid.Column>
 
-                        <Grid.Column widescreen={16} computer={16} tablet={8} mobile={16}>
-                            <Grid.Row>
-                                <ChangePassword/>
-                            </Grid.Row>
-                        </Grid.Column>
+                            <Grid.Column widescreen={16} computer={16} tablet={8} mobile={16}>
+                                <Grid.Row>
+                                    <ChangeEmail/>
+                                </Grid.Row>
+                            </Grid.Column>
 
-                        <Grid.Column widescreen={16} computer={16} tablet={8} mobile={16}>
-                            <Grid.Row>
-                                <ChangeEmail/>
-                            </Grid.Row>
-                        </Grid.Column>
+                        </Grid>
+                    </Grid.Column>
 
-                    </Grid>
-                </Grid.Column>
-
-                <Grid.Column widescreen={9} computer={9} tablet={16} mobile={16}>
-                    <Grid.Row>
-                        <Identification />
-                    </Grid.Row>
-                </Grid.Column>
-
+                    <Grid.Column widescreen={9} computer={9} tablet={16} mobile={16}>
+                        <Grid.Row>
+                            <Identification />
+                        </Grid.Row>
+                    </Grid.Column>
+                </Grid>
             </Grid.Row>
         </Grid>
     </Container>
