49ab9e86b3660fe5c6a5cf8dc7a40ce3557b1a4a
German <ueuknjg@gmail.com>
2018-02-26 17:21:03 +0300

    replace server URL

--- src/libs/ApiLib/SignUp.js
+++ src/libs/ApiLib/SignUp.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 class SignUpLib {
-    static url = "http://192.168.0.136:4874/v1/";
+    static url = "http://159.89.10.197:4874/v1/";
     static checkEmailURL = "profile/availability?email";
     static regUserURL = "profile";
     static checkAvailability(email) {
