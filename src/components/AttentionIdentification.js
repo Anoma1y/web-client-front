1a86ed52871ad7283f8ff6a04728b4553c47ab53
German <ueuknjg@gmail.com>
2018-02-27 17:48:59 +0300

    fixed style for header menu

--- src/components/AttentionIdentification.js
+++ src/components/AttentionIdentification.js
@@ -15,12 +15,16 @@ export const AttentionIdentification = () => {
                         <div className={"attentionIdentification__container"}>
                             <Grid>
                                 <Grid.Row>
+                                    <Grid.Column width={1}>
+                                    </Grid.Column>
                                     <Grid.Column widescreen={1} computer={1}  tablet={1} mobile={16} textAlign={"center"}>
                                        <Icon name={"warning sign"} className={"attentionIdentification__icon"}/>
                                     </Grid.Column>
-                                    <Grid.Column widescreen={15} computer={15} tablet={15} mobile={16} className={"attentionIdentification__textColumn"}>
+                                    <Grid.Column widescreen={13} computer={13} tablet={13} mobile={16} className={"attentionIdentification__textColumn"}>
                                         <span className={"attentionIdentification__text"}>In order to be able to buy tokens, you need to be <Link to={"/dashboard/settings"} className={"attentionIdentification__link"}>identified</Link>
                                         </span>
+                                    </Grid.Column>
+                                    <Grid.Column width={1}>
                                     </Grid.Column>
                                 </Grid.Row>
                             </Grid>
--- src/components/HeaderMenu/index.js
+++ src/components/HeaderMenu/index.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import {
     Menu,
-    Icon
+    Icon,
+    Container,
+    Grid
 } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 import Logo from 'logo.svg';
-import HeaderMenuItem from './HeaderMenuItem';
+// import HeaderMenuItem from './HeaderMenuItem';
 
 class HeaderMenu extends Component {
     constructor(props) {
@@ -27,26 +30,69 @@ class HeaderMenu extends Component {
         };
     }
     render() {
-        const { pathname } = this.props.routing.location;
-        const { menu } = this.state;
-
+        const { toggleSidebar } = this.props;
         return (
                 <Menu className={"header__menu"}>
-                    <Menu.Item className={"header__menu_logo"}>
-                        <a href={"http://www.google.com"}>
-                            <img src={Logo} alt="Logo" />
-                        </a>
-                    </Menu.Item>
-                    { menu.map((item, index) => <HeaderMenuItem key={index} LinkName={item["name"]} href={item["href"]} activeLink={pathname} iconName={item["iconName"]}/> )}
-                    {localStorage.jwt ? <HeaderMenuItem LinkName={"Logout"} href={"/logout"} activeLink={pathname} iconName={"log out"}/> : null}
-                    <Menu.Item className={"mobileMenu__trigger"}>
-                        <Icon name={"bars"} onClick={this.props.toggleSidebar}/>
-                    </Menu.Item>
+                    <Container>
+                        <Grid className={"grid__menu"}>
+                            <Grid.Row>
+                                <Grid.Column width={1}>
+                                </Grid.Column>
+                                <Grid.Column floated={"left"} className={"logo_content"} width={3}>
+                                    <Menu className={"menu__color"}>
+                                        <Menu.Item>
+                                            <Link to={"/dashboard/"}>
+                                                <img src={Logo} alt="Logo" />
+                                            </Link>
+                                        </Menu.Item>
+                                    </Menu>
+                                </Grid.Column>
+                                <Grid.Column floated={"right"} width={6}>
+                                    {localStorage.jwt ?
+                                        <Menu className={"menu__color user__menu"}>
+                                            <Menu.Item className={"user__menu_item user__menu-email"}>
+                                                <Link to={"/settings"} >
+                                                    mail@mail.ru
+                                                </Link>
+                                            </Menu.Item>
+                                            <Menu.Item className={"user__menu_item user__menu-logout"}>
+                                                <Link to={"/logout"} >
+                                                    Log out
+                                                </Link>
+                                            </Menu.Item>
+                                        </Menu>
+                                    : null}
+                                </Grid.Column>
+                                <Grid.Column width={1}>
+                                </Grid.Column>
+                            </Grid.Row>
+                        </Grid>
+                    </Container>
                 </Menu>
         );
     }
 }
-
+{/*<Menu.Item className={"header__menu_logo"}>*/}
+    {/*<a href={"http://www.google.com"}>*/}
+        {/*<img src={Logo} alt="Logo" />*/}
+    {/*</a>*/}
+{/*</Menu.Item>*/}
+{/*<Menu.Item>*/}
+{/*{localStorage.jwt ?*/}
+    {/*<Menu.Item>*/}
+        {/*<Menu.Item name={"Settings"}>*/}
+            {/*<Link to={"/settings"} >*/}
+                {/*mail@mail.ru*/}
+            {/*</Link>*/}
+        {/*</Menu.Item>*/}
+        {/*<Menu.Item name={"logout"}>*/}
+            {/*<Link to={"/logout"} >*/}
+                {/*Log out*/}
+            {/*</Link>*/}
+        {/*</Menu.Item>*/}
+    {/*</Menu.Item>*/}
+    {/*: null}*/}
+{/*</Menu.Item>*/}
 export default connect(state => ({ routing: state.routing }), {
 
 })(HeaderMenu);
