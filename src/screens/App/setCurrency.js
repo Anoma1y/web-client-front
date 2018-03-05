03f549e1c4e12ac86978e76d63be104dc75c87db
German <ueuknjg@gmail.com>
2018-03-05 16:50:12 +0300

    add google tag manager

---
+++ src/screens/App/setCurrency.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import CryptoCurrency from 'libs/ApiLib/CryptoCurrency';
+
+class setCurrency extends Component {
+
+
+    componentWillMount() {
+
+    }
+
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default setCurrency;
