48e8f5a33a752418b95db65abf7fd344c2d974a3
German <ueuknjg@gmail.com>
2018-03-06 12:37:03 +0300

    widget zopim moved (App.js -> index.html)

--- src/screens/App/index.js
+++ src/screens/App/index.js
@@ -12,24 +12,11 @@ import Logout from './Logout';
 import Redirect from './Redirect';
 
 class App extends Component {
-    componentDidMount() {
-        const script = document.createElement('script');
-        script.type = 'text/javascript';
-        script.async = true;
-        script.innerText = `
-            window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s=
-            d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
-            _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute("charset","utf-8");
-            $.src="https://v2.zopim.com/?5WAfwmvEvuizK4ZNTuBBNO9RVYepv5Ai";z.t=+new Date;$.
-            type="text/javascript";e.parentNode.insertBefore($,e)})(document,"script");
-        `;
-        this.scriptNode.appendChild(script);
-    }
+
      render () {
         return (
             <div>
                 <CheckToken />
-                <div ref={node => (this.scriptNode = node)} className={"sads"}/>
                 <main>
                     <HeaderMenu/>
                     <Switch>
