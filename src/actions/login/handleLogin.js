57dd69d7e2cccafae44c765e8647235d2ada0fcc
German <ueuknjg@gmail.com>
2018-03-06 15:49:02 +0300

    fixed login and initial user

--- src/actions/login/handleLogin.js
+++ src/actions/login/handleLogin.js
@@ -10,25 +10,28 @@ import {
 
 export const handleLogin = value => {
     return dispatch => {
-        const { email, password } = value;
+        const { 
+            email, 
+            password 
+        } = value;
         dispatch(setAuthInProgress(true));
         Login.logUser(email, password).then((data) => {
             dispatch(setError(null));
-            const { jwt } = data.data;
+            const {
+                jwt
+            } = data.data;
             Login.getUser(data.data.jwt).then((user) => {
                 const {
                     is_kyc_passed,
                     email,
                     roles
                 } = user.data;
-                localStorage.setItem("roles", roles);
-                localStorage.setItem("jwt", jwt);
+                dispatch(handleTokenUser({jwt, roles}));
                 dispatch(initIdenfified(is_kyc_passed));
                 dispatch(initEmail(email));
+                dispatch(setAuthInProgress(false));
+                dispatch(push('/dashboard/'));
             });
-            dispatch(handleTokenUser(jwt));
-            dispatch(setAuthInProgress(false));
-            dispatch(push('/dashboard/'));
         }).catch((err) =>{
             dispatch(setAuthInProgress(false));
             dispatch(setError(err));
