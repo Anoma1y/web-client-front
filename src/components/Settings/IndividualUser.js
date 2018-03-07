ca40748888d6dee18cde5a965fe06152ff55e553
German <ueuknjg@gmail.com>
2018-03-07 14:20:10 +0300

    fixed style

--- src/components/Settings/IndividualUser.js
+++ src/components/Settings/IndividualUser.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Grid } from 'semantic-ui-react';
+import {
+    Grid,
+    Divider
+} from 'semantic-ui-react';
 import IdentificationImgUpload from './IdentificationImgUpload';
 import PersonInformation from './PersonInformation';
 import { SettingsButton } from './SettingsButton';
@@ -9,15 +12,21 @@ class IndividualUser extends Component {
 
     renderUploadInfo = () => {
         const { individualUser } = this.props.settings;
-        return individualUser.map(item => {
+        return individualUser.map((item, index) => {
             return (
                 <Grid.Row key={item.id}>
-                    <Grid.Column>
+                    <Grid.Column width={16}>
                         <IdentificationImgUpload
                             description={item.description}
                             id={item.id}
                         />
                     </Grid.Column>
+                    { index !== (individualUser.length - 1) ?
+                        <Grid.Column width={16}>
+                            <Divider/>
+                        </Grid.Column>
+                        : null
+                    }
                 </Grid.Row>
             )
         })
