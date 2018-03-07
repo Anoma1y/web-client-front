4766839c0af089403c52d96b65f1b14066980384
German <ueuknjg@gmail.com>
2018-03-07 18:38:01 +0300

    add redirect to dashboard if user auth

--- src/actions/users/initialUser.js
+++ src/actions/users/initialUser.js
@@ -1,4 +1,5 @@
-import Login from 'libs/ApiLib/Login'
+import Login from 'libs/ApiLib/Login';
+import { push } from 'react-router-redux';
 import {
     initIdenfified,
     initEmail,
@@ -7,16 +8,20 @@ import {
     putRoles
 } from 'actions/users';
 import { redirectToSignup } from 'actions/redirect';
+
 export const initialUser = token => {
-    return dispatch => {
+    return (dispatch, getState) => {
         Login.getUser(token).then((user) =>{
             const { email, is_kyc_passed, roles} = user.data;
-            //TODO добавить проверку роли и если "Админ" - запрерить перенаправление на дашборд
             dispatch(initIdenfified(is_kyc_passed));
             localStorage.setItem("roles", roles);
             dispatch(initEmail(email));
             dispatch(putToken(token));
-            dispatch(putRoles(roles))
+            dispatch(putRoles(roles));
+            const { pathname: PATH } = getState().routing.location;
+            if (PATH === "/" || PATH === "/signup" || PATH === "/login") {
+                dispatch(push('/dashboard'));
+            }
         }).catch(() => {
             dispatch(deleteToken());
             localStorage.removeItem("jwt");
