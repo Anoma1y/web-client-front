c02e40290d4e407b2a811daf09cd044c985a030d
German <ueuknjg@gmail.com>
2018-03-12 15:32:59 +0300

    fixed style for Request Btn and replace number name (status) -> string name (status)

--- src/components/Admin/ApplicationTableRow.js
+++ src/components/Admin/ApplicationTableRow.js
@@ -31,23 +31,30 @@ class ApplicationTableRow extends Component {
             profile,
             status
         } = this.props;
+
         const {
             currency: cryptoCurrency,
             TSR: TOKEN_ATTITUDE_ETH,
             bonus
         } = this.props.calculator;
+
         const fixedCurrency = currency.split("/");
+
         const {
             TOKENVALUE,
             CURRENCYVALUE
         } = applicationCalc(amount, fixedCurrency, TOKEN_ATTITUDE_ETH, cryptoCurrency, bonus);
+
         const { applicationStatus } = this.state;
+
         const fixToken = fixedCurrency[0] === "TSR" ?
             <Table.Cell width={1} positive>{TOKENVALUE}</Table.Cell> :
             <Table.Cell width={1}>{TOKENVALUE}</Table.Cell>;
+
         const fixCurrency = fixedCurrency[0] !== "TSR" ?
             <Table.Cell width={1} positive>{CURRENCYVALUE}</Table.Cell> :
             <Table.Cell width={1} >{CURRENCYVALUE}</Table.Cell>;
+
         return (
             <Table.Row>
                 <Table.Cell width={1}>{id}</Table.Cell>
@@ -57,56 +64,58 @@ class ApplicationTableRow extends Component {
                 <Table.Cell width={1}>Russia</Table.Cell>
                 <Table.Cell width={3}>{profile.email}</Table.Cell>
                 <Table.Cell width={1}>{profile.is_kyc_passed === false ? "No" : "Yes"}</Table.Cell>
-                <Table.Cell width={1}>
-                    <Modal
-                        trigger={<span>{status}</span>}
-                        size={"tiny"}
-                    >
-                        <Modal.Content className={"modal__success"}>
-                            <Modal.Description>Change Status</Modal.Description>
+                <Modal
+                    trigger={
+                        <Table.Cell width={1} className={"cursor-pointer"}>
+                            <span>{status === 0 ? "Awaiting" : status === 1 ? "Approved" : status === 3 ? "Rejected" : "Paid"}</span>
+                        </Table.Cell>
+                    }
+                    size={"tiny"}
+                >
+                    <Modal.Content className={"modal__success"}>
+                        <Modal.Description>Change Status</Modal.Description>
 
-                            <Modal.Description>
-                                <Form>
-                                    <Form.Field>
-                                        Current status: <b>{applicationStatus === 0 ? "Awaiting" : applicationStatus === 1 ? "Approved" : "Rejected"}</b>
-                                    </Form.Field>
-                                    <Form.Field>
-                                        <Radio
-                                            label='Awaiting'
-                                            name='applicationStatusGroup'
-                                            value='0'
-                                            checked={this.state.applicationStatus === 0}
-                                            onChange={this.handleChange}
-                                        />
-                                    </Form.Field>
-                                    <Form.Field>
-                                        <Radio
-                                            label='Approved'
-                                            name='applicationStatusGroup'
-                                            value='1'
-                                            checked={this.state.applicationStatus === 1}
-                                            onChange={this.handleChange}
-                                        />
-                                    </Form.Field>
-                                    <Form.Field>
-                                        <Radio
-                                            label='Rejected'
-                                            name='applicationStatusGroup'
-                                            value='2'
-                                            checked={this.state.applicationStatus === 3}
-                                            onChange={this.handleChange}
-                                        />
-                                    </Form.Field>
-                                    <Form.Field>
-                                        <Button>
-                                            Change
-                                        </Button>
-                                    </Form.Field>
-                                </Form>
-                            </Modal.Description>
-                        </Modal.Content>
-                    </Modal>
-                </Table.Cell>
+                        <Modal.Description>
+                            <Form>
+                                <Form.Field>
+                                    Current status: <b>{status === 0 ? "Awaiting" : status === 1 ? "Approved" : status === 3 ? "Rejected" : "Paid"}</b>
+                                </Form.Field>
+                                <Form.Field>
+                                    <Radio
+                                        label='Awaiting'
+                                        name='applicationStatusGroup'
+                                        value='0'
+                                        checked={this.state.applicationStatus === 0}
+                                        onChange={this.handleChange}
+                                    />
+                                </Form.Field>
+                                <Form.Field>
+                                    <Radio
+                                        label='Approved'
+                                        name='applicationStatusGroup'
+                                        value='1'
+                                        checked={this.state.applicationStatus === 1}
+                                        onChange={this.handleChange}
+                                    />
+                                </Form.Field>
+                                <Form.Field>
+                                    <Radio
+                                        label='Rejected'
+                                        name='applicationStatusGroup'
+                                        value='2'
+                                        checked={this.state.applicationStatus === 3}
+                                        onChange={this.handleChange}
+                                    />
+                                </Form.Field>
+                                <Form.Field>
+                                    <Button>
+                                        Change
+                                    </Button>
+                                </Form.Field>
+                            </Form>
+                        </Modal.Description>
+                    </Modal.Content>
+                </Modal>
                 <Table.Cell width={4} className={"admin__application_comment"}>{comment}</Table.Cell>
                 <Table.Cell width={1}>
                     <Checkbox/>
--- src/components/Admin/UserTableRow.js
+++ src/components/Admin/UserTableRow.js
@@ -31,48 +31,46 @@ class UserTableRow extends Component {
                 <Table.Cell>{id}</Table.Cell>
                 <Table.Cell>{moment(createdAt).format('DD-MM-YYYY h:mm:ss')}</Table.Cell>
                 <Table.Cell>{email}</Table.Cell>
-                <Table.Cell>
-                    <Modal
-                        trigger={<span>{roles}</span>}
-                        size={"tiny"}
-                    >
-                        <Modal.Content className={"modal__success"}>
-                            <Modal.Description>
-                                <h1>Change Role</h1>
-                            </Modal.Description>
-                            <Modal.Description>
-                                <Form>
-                                    <p>
-                                        Current role: <b>{this.props.roles}</b>
-                                    </p>
-                                    <Form.Field>
-                                        <Radio
-                                            label='Admin'
-                                            name='roleGroup'
-                                            value='admin'
-                                            checked={this.state.userRole === 'admin'}
-                                            onChange={this.handleChange}
-                                        />
-                                    </Form.Field>
-                                    <Form.Field>
-                                        <Radio
-                                            label='User'
-                                            name='roleGroup'
-                                            value='user'
-                                            checked={this.state.userRole === 'user'}
-                                            onChange={this.handleChange}
-                                        />
-                                    </Form.Field>
-                                    <Form.Field>
-                                        <Button>
-                                            Change
-                                        </Button>
-                                    </Form.Field>
-                                </Form>
-                            </Modal.Description>
-                        </Modal.Content>
-                    </Modal>
-                </Table.Cell>
+                <Modal
+                    trigger={<Table.Cell className={"cursor-pointer"}><span>{roles}</span></Table.Cell>}
+                    size={"tiny"}
+                >
+                    <Modal.Content className={"modal__success"}>
+                        <Modal.Description>
+                            <h1>Change Role</h1>
+                        </Modal.Description>
+                        <Modal.Description>
+                            <Form>
+                                <p>
+                                    Current role: <b>{this.props.roles}</b>
+                                </p>
+                                <Form.Field>
+                                    <Radio
+                                        label='Admin'
+                                        name='roleGroup'
+                                        value='admin'
+                                        checked={this.state.userRole === 'admin'}
+                                        onChange={this.handleChange}
+                                    />
+                                </Form.Field>
+                                <Form.Field>
+                                    <Radio
+                                        label='User'
+                                        name='roleGroup'
+                                        value='user'
+                                        checked={this.state.userRole === 'user'}
+                                        onChange={this.handleChange}
+                                    />
+                                </Form.Field>
+                                <Form.Field>
+                                    <Button>
+                                        Change
+                                    </Button>
+                                </Form.Field>
+                            </Form>
+                        </Modal.Description>
+                    </Modal.Content>
+                </Modal>
                 <Table.Cell>{is_verified === false ? "No" : "Yes"}</Table.Cell>
                 <Table.Cell>{is_kyc_passed === false ? "No" : "Yes"}</Table.Cell>
                 <Table.Cell>
